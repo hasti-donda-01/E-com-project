929d225e271e262d6e01db41d077aaf0d0b05ec9
Hasti Donda <hastidonda05@gmail.com>
2026-03-29 20:15:09 +0530

    Added gitignore file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello")
+import env from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log("server running");
+})
